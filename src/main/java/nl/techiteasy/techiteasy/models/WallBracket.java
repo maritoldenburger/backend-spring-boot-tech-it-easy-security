031ab9b99535e2f118b885206a25625b211744c7
moldenburger <maritoldenburger@hotmail.com>
2025-10-12 15:12:17 +0200

    Models voor RemoteController, CI-Module en WallBracket aangemaakt

---
+++ src/main/java/nl/techiteasy/techiteasy/models/WallBracket.java
@@ -0,0 +1,58 @@
+package nl.techiteasy.techiteasy.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "wall_brackets")
+public class WallBracket {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String size;
+    private Boolean adjustable;
+    private String name;
+    private Double price;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public Boolean getAdjustable() {
+        return adjustable;
+    }
+
+    public void setAdjustable(Boolean adjustable) {
+        this.adjustable = adjustable;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+}
