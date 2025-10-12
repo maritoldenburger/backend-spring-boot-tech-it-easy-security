4f28074709e63031df29d7abc14ef1c98f13047d
moldenburger <maritoldenburger@hotmail.com>
2025-10-12 15:18:36 +0200

    Repositories voor RemoteController, CI-Module en WallBracket aangemaakt

---
+++ src/main/java/nl/techiteasy/techiteasy/repositories/CIModuleRepository.java
@@ -0,0 +1,8 @@
+package nl.techiteasy.techiteasy.repositories;
+
+import nl.techiteasy.techiteasy.models.CIModule;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CIModuleRepository extends JpaRepository<CIModule, Long> {
+
+}
---
+++ src/main/java/nl/techiteasy/techiteasy/repositories/RemoteControllerRepository.java
@@ -0,0 +1,7 @@
+package nl.techiteasy.techiteasy.repositories;
+
+import nl.techiteasy.techiteasy.models.RemoteController;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RemoteControllerRepository extends JpaRepository<RemoteController, Long> {
+}
