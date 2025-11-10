ca45bfa7b87038266e9bcc78f9a420541b35d60c
moldenburger <maritoldenburger@hotmail.com>
2025-11-10 16:51:22 +0100

    pom.xml aangepast, User, Authority en de AuthorityKey toegevoegd en fouten opgelost

---
+++ src/main/java/nl/techiteasy/techiteasy/models/AuthorityKey.java
@@ -0,0 +1,10 @@
+package nl.techiteasy.techiteasy.models;
+
+import java.io.Serializable;
+
+public class AuthorityKey implements Serializable {
+    private String username;
+    private String authority;
+
+    public AuthorityKey() {}
+}
